3063b8dff1b5c48134d169d7cac801362bab978e
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-05-29 09:55:05 -0700

    Add geocode with mongo cache

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ include Mongo
 mongo = MongoClient.new
 db = mongo['irrigator']
 stations = db['ghcnd_stations']
+geocodes = db['geocode_cache']
 
 # mongo indexes I should probably be running:
 # db.ghcnd_stations.ensureIndex({ id: 1 }, {unique: true })
@@ -24,10 +25,34 @@ get '/nearest.json' do
       '$maxDistance' => 250000, # 250 km
     }
   }
-  output = []
-  stations.find({ 'loc' => conditions }, { :limit => 10 }).each do |doc|
-    output.push(doc)
+  json stations.find({ :loc => conditions }, { :limit => 10 }).to_a
+end
+
+get '/geocode.json' do
+  addr = params[:addr].strip
+  output = geocodes.find_one :addr => addr
+  if ! output
+    output = { :addr => addr }
+    results = Geocoder.search addr
+    if results[0].nil?
+      output[:error] = true,
+      output[:userMessage] = 'Geocoding failed'
+    else
+      output[:lat] = results[0].latitude
+      output[:lng] = results[0].longitude
+      output[:precision] = results[0].precision
+      geocodes.insert(output)
+    end
   end
+
+  # TODO: exclude "_id" parameter
+  json output
+end
+
+get 'precip.json' do
+  station = params[:station]
+  # TODO: query CDO api for specified station, return json
 
   json output
 end
+
